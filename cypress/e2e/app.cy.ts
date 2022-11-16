50d992f5b614f56d5292918b009c9409b4d404ba
Delphin <batantoud@gmail.com>
2022-11-16 12:12:02 +0100

    commit

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -15,7 +15,6 @@ describe('Navigation', () => {
     cy.url().should('include', '/about')
 
     // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
   })
 })
 
